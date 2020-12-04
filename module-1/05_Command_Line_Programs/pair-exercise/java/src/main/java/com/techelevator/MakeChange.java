cad1199065f0df25b69f2f822dc19202a8753974
Michael Drew <michaeljdrew14@gmail.com>
2020-12-04 16:46:02 -0500

    group work

--- module-1/05_Command_Line_Programs/pair-exercise/java/src/main/java/com/techelevator/MakeChange.java
+++ module-1/05_Command_Line_Programs/pair-exercise/java/src/main/java/com/techelevator/MakeChange.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then
  display the change required.
@@ -9,10 +11,20 @@ package com.techelevator;
  Please enter the amount tendered: 100.00
  The change required is 76.35
  */
-public class MakeChange {
-
-	public static void main(String[] args) {
-
-	}
-
+	public class MakeChange {
+		
+		public static void main(String[] args) {
+			 Scanner scanner = new Scanner(System.in);
+			 System.out.println("Welcome to the Change Calculator");
+			 System.out.print("Enter the bill amount: ");
+			 String makeChangeString = scanner.nextLine();
+			 double makeChange = Double.parseDouble(makeChangeString);
+			 System.out.println("The bill amount you entered as a decimal is " + makeChange);
+			 System.out.print("Please enter the amount you paid: ");
+			 String paidAmountString = scanner.nextLine();
+			 double paidAmount = Double.parseDouble(paidAmountString);
+			 double change = paidAmount - makeChange;
+			 System.out.println("Here is your change: " + "$" + change);
+		}
+	
 }
--- module-1/05_Command_Line_Programs/pair-exercise/java/src/main/java/com/techelevator/MartianWeight.java
+++ module-1/05_Command_Line_Programs/pair-exercise/java/src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  In case you've ever pondered how much you weight on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -16,10 +18,32 @@ Enter a series of Earth weights (space-separated): 98 235 185
  235 lbs. on Earth, is 88 lbs. on Mars.
  185 lbs. on Earth, is 69 lbs. on Mars. 
  */
-public class MartianWeight {
 
+public class MartianWeight {
 	public static void main(String[] args) {
 
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Weight on Earth and Mars");
+	System.out.print("Enter weights in pounds (space seperated): ");
+			String weightString = scanner.nextLine();
+			String[] weightsAsIndividualStrings = weightString.split(" ");
+			double[] weights = new double[weightsAsIndividualStrings.length];
+			double[] martianWeights = new double[weightsAsIndividualStrings.length];
+			for(int i = 0;i<weightsAsIndividualStrings.length;i++){
+				String stringToParse = weightsAsIndividualStrings[i];
+				double weight = Double.parseDouble(stringToParse);
+				weights[i] = weight;
+							}
+			for(int i = 0;i<weights.length;i++) {
+				double martianWeight = weights[i] * .378;
+				martianWeights[i] = martianWeight;
+			}
+				System.out.println("Your weights on Earth and Mars are:");
+				for(int j = 0;j<martianWeights.length;j++)
+				{
+
+					System.out.println(weights[j] + " lbs. on Earth, is " + martianWeights[j] + " lbs. on Mars.");
+				}System.out.println();
+			} 
 	}
 
-}
