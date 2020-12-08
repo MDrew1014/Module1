469e4494cd7c9bb619a8b3c5c3b203bad28f3ed0
Michael Drew <michaeljdrew14@gmail.com>
2020-12-08 08:44:14 -0500

    Still having trouble with some of these command line programs

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
@@ -4,27 +4,14 @@ import java.util.Scanner;
 
 public class DecimalToBinary {
 
-	public static void main(String[] args) {
-		 Scanner scanner = new Scanner(System.in);    
-		
-        System.out.print("Please enter in a series of decimal values (separated by spaces):");
-        decimal = scanner.nextInt();
-		
-        quot = decimal;
-		
-        while(quot != 0)
-        {
-            binary[i++] = quot%2;
-            quot = quot/2;
-        }
-		
-        System.out.print("Binary number is: ");
-        for(j=i-1; j>0; j--)
-        {
-            System.out.print(binary[j]);
-        }
-        System.out.print("\n");
-    }
-	}
-
+	public static void main(String[] args) {Scanner sc = new Scanner(System.in);
+	int number;
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.println("Please enter in a series of decimal values (separated by spaces):");
+	number = scanner.nextInt();
+	
+	System.out.println("Binary number is " + Integer.toBinaryString(number));
+}
+}
 
