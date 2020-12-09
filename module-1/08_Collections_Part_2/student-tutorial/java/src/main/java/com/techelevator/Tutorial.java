4307b6580ad9f8de33759b587656f78804ca4730
Michael Drew <michaeljdrew14@gmail.com>
2020-12-08 22:18:33 -0500

    tutorial 8

--- module-1/08_Collections_Part_2/student-tutorial/java/src/main/java/com/techelevator/Tutorial.java
+++ module-1/08_Collections_Part_2/student-tutorial/java/src/main/java/com/techelevator/Tutorial.java
@@ -9,13 +9,26 @@ public class Tutorial {
     public static void main(String[] args) {
 
         // Step One: Declare a Map
-
+    	Map<String, Boolean> dogs = new HashMap<String, Boolean>();
     	
     	// Step Two: Add items to a Map
-
+    	dogs.put("Stuart", true);
+    	dogs.put("Jaxson", true);
+    	dogs.put("Olivia", true);
+    	dogs.put("Harry", false);
+    	dogs.put("Sally", false);
+    	
     	
     	// Step Three: Loop through a Map
 
+    for(Map.Entry<String, Boolean> dog : dogs.entrySet()) {
+    	if(dog.getValue()) {
+    		System.out.println(dog.getKey() + " is a dog.");
+    	}else {
+    		System.out.println(dog.getKey() + " is a cat.");
+    	}
     }
-
+    }
 }
+
+
