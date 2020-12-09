d9b786788c66ff2fad7626f7dc1c00a977934bec
Michael Drew <michaeljdrew14@gmail.com>
2020-12-09 12:46:32 -0500

    soemthing

--- module-1/08_Collections_Part_2/student-lecture/java/src/main/java/com/techelevator/Lecture.java
+++ module-1/08_Collections_Part_2/student-lecture/java/src/main/java/com/techelevator/Lecture.java
@@ -3,6 +3,8 @@ package com.techelevator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Lecture {
 
@@ -12,8 +14,39 @@ public class Lecture {
 		System.out.println("        MAPS");
 		System.out.println("####################");
 		System.out.println();
-
-
+		
+		//Tracks inventory for player
+		Map<String, Integer> inventory = new HashMap<String, Integer>();
+		
+		
+		Map<String,List<String>> players = new HashMap<String,List<String>>();
+		
+		//load up inventory with put
+		inventory.put("pokeball", 10);
+		inventory.put("potion", 5);
+		inventory.put("rare candy",1);
+		
+		//replace an item
+		inventory.put("pokeball", 15);
+		
+		//for comparison, we'll make a list for out player
+		List<String> ourPlayer = new ArrayList<String>();
+		ourPlayer.add("Charmander");
+		ourPlayer.add("e");
+		
+		
+		
+		Map<String,Double> myTeamsHealth = new HashMap<String,Double>();
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
