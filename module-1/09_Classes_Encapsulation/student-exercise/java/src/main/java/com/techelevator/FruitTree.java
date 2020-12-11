f1959585dd20312ea8dcecc5764f9b631d61cda8
Michael Drew <michaeljdrew14@gmail.com>
2020-12-10 22:51:59 -0500

    Exercise 9 halfway point

--- module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/FruitTree.java
+++ module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/FruitTree.java
@@ -1,5 +1,31 @@
 package com.techelevator;
 
 public class FruitTree {
-
+	
+	
+	//Instance Variables
+	private String typeOfFruit; // Type of fruit on the tree
+	private  int piecesOfFruitLeft;
+	
+	
+	
+	public FruitTree(String typeOfFruit,int startingPiecesOfFruit) {
+		this.typeOfFruit = typeOfFruit;
+		this.piecesOfFruitLeft = startingPiecesOfFruit;
+	}
+	public boolean pickFruit(int numberOfPiecesToRemove) {
+		if (numberOfPiecesToRemove <= piecesOfFruitLeft){
+			piecesOfFruitLeft -= numberOfPiecesToRemove;
+			return true;
+		}else{   return false;
+		}
+	}
+	public String getTypeOfFruit() {
+		return typeOfFruit;
+	}
+	public int getPiecesOfFruitLeft() {
+		return piecesOfFruitLeft;
+	}
+	
+	
 }
--- module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/HomeworkAssignment.java
+++ module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -2,4 +2,55 @@ package com.techelevator;
 
 public class HomeworkAssignment {
 
+	// Instance Variables
+	private int earnedMarks; // the total correct marks submitter recieved on the assignment
+	private int possibleMarks;// The number of possible marks on the assignment
+	private String submitterName;// The submitter's name for the assignment
+	// Method
+
+	public String getLetterGrade() {
+		double percentage = (double) earnedMarks / possibleMarks;
+
+		if (percentage >= 0.9) {
+			return "A";
+		} else if (percentage >= 0.8) {
+			return "B";
+		} else if (percentage >= 0.7) {
+			return "C";
+		} else if (percentage >= 0.6) {
+			return "D";
+		} else {
+			return "F";
+		}
+	}
+
+	// Constructor
+	public HomeworkAssignment(int possibleMarks, String submitterName) {
+		this.possibleMarks = possibleMarks;
+		this.submitterName = submitterName;
+
+	}
+
+	// getters and setters
+	public int getEarnedMarks() {
+		return earnedMarks;
+	}
+
+	public void setEarnedMarks(int earnedMarks) {
+		if (earnedMarks <= possibleMarks && earnedMarks >= 0) {
+			this.earnedMarks = earnedMarks;
+		}
+	}
+
+	public int getPossibleMarks() {
+		return possibleMarks;
+	}
+
+
+	public String getSubmitterName() {
+		return submitterName;
+	}
+
+	
+
 }
