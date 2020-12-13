3281619bfee4a36b71ec1fd72aadfd73e48de4ac
Michael Drew <michaeljdrew14@gmail.com>
2020-12-13 11:51:34 -0500

    09_Classes_Encapsulation finished finally

--- module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Elevator.java
+++ module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Elevator.java
@@ -5,55 +5,47 @@ public class Elevator {
 	private int currentFloor;
 	private int numberOfFloors;
 	private boolean doorOpen;
-	//constant
-	public static final int defaultCurrentFloor = 1;
-	//constructor
-	public Elevator(int numberOfLevels){
+	// constant
+
+	// constructor
+	public Elevator(int numberOfLevels) {
 		this.numberOfFloors = numberOfLevels;
+		this.currentFloor = 1;
 	}
-	//methods
+
+	// methods
 	public void openDoor() {
 		doorOpen = true;
 	}
+
 	public void closeDoor() {
 		doorOpen = false;
 	}
+
 	public void goUp(int desiredFloor) {
-		if (desiredFloor <= numberOfFloors) {
-				if (desiredFloor > currentFloor) {
-				currentFloor = desiredFloor;
-				}
-			}
+		if (!doorOpen && desiredFloor <= numberOfFloors && desiredFloor > currentFloor) {
+
+			currentFloor = desiredFloor;
 		}
-	
-	
+	}
+
 	public void goDown(int desiredFloor) {
-		if (!doorOpen){
-			if (desiredFloor >= defaultCurrentFloor) {
-				if (desiredFloor < currentFloor) {
-				currentFloor = desiredFloor;
-				}
-			}
+		if (!doorOpen && desiredFloor < currentFloor && desiredFloor > 0) {
+			currentFloor = desiredFloor;
 		}
 	}
-	///getters and setters
+
+	/// getters and setters
 	public int getCurrentFloor() {
-		return defaultCurrentFloor;
+		return currentFloor;
 	}
+
 	public int getNumberOfFloors() {
 		return numberOfFloors;
 	}
+
 	public boolean isDoorOpen() {
 		return doorOpen;
 	}
-	
-	
-	
-	
-	
-	}
-	
-	
-	
 
-	
+}	
