8d22ec99878d2e20831e8ce33d99d7ffc0563e57
Michael Drew <michaeljdrew14@gmail.com>
2021-02-25 12:12:47 -0500

    lecture

---
+++ module-3/14_Vue_Routing/student-lecture/src/views/ProductDetail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id='app' class='main'>
+      <h1>{{product.name}}</h1>
+     <p class='description'>{{product.description}}</p>
+     <a href="#">Back to products</a>&nbsp;
+     <a href="#">Add Review</a>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    const activeProductId = this.$route.params.id;
+    this.$store.commit("SET_ACTIVE_PRODUCT", activeProductId);
+  },
+  computed: {
+    product() {
+      const products = this.$store.state.products;
+      const product = products.find((p) => {
+        return p.id == this.$store.state.activeProduct;
+      });
+      return product;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
