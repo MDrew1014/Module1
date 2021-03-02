f1a3992b8b5eeacf35eaedcc513013ca57f88c8e
Michael Drew <michaeljdrew14@gmail.com>
2021-03-02 16:34:39 -0500

    hw complete

--- module-3/17_Web_Services_POST/student-exercise/src/services/TopicService.js
+++ module-3/17_Web_Services_POST/student-exercise/src/services/TopicService.js
@@ -12,6 +12,18 @@ export default {
 
   get(id) {
     return http.get(`/topics/${id}`);
+  },
+  create(topic){
+    return http.post('/topics', topic);
+  },
+  delete(id){
+    return http.delete(`/topics/${id}`);
+  },
+  update(id, topic){
+    return http.put(`/topics/${id}`,topic);
+  },
+  deleteMessage(id){
+    return http.delete(`/messages/${id}`);
   }
 
 }
